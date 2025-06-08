5bfde5b0d0529da190d1c4b60f2ac47c6c4593ba
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2025-06-08 17:38:19 +0200

    add interface names

--- plugin/src/main/kotlin/dev/mmrl/Module.kt
+++ plugin/src/main/kotlin/dev/mmrl/Module.kt
@@ -5,6 +5,8 @@ import com.dergoogler.mmrl.webui.interfaces.WXInterface
 import com.dergoogler.mmrl.webui.interfaces.WXOptions
 
 class Module(wxOptions: WXOptions) : WXInterface(wxOptions) {
+    override var name = "module"
+    override var tag = "Module"
 
     @get:JavascriptInterface
     val id get() = modId.toString()
